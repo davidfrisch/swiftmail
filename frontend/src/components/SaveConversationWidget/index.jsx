99104fcd1c8d2200097e8ee2f7d9bebcc26b5617
David Frischer <david.frischer@frischer.ch>
2024-06-12 13:36:02 +0100

    [frontend] Finis first sketch of frontend

--- frontend/src/components/SaveConversationWidget/index.jsx
+++ frontend/src/components/SaveConversationWidget/index.jsx
@@ -1,7 +1,20 @@
+import { Input, Typography, Button } from "antd";
+import "./styles.css";
+
+const { TextArea } = Input;
+const { Title, Text } = Typography;
+
 const SaveConversationWidget = () => {
-  return ( <div>
-    Save Conversation
-    </div> );
-}
- 
+  return (
+    <div className="save-conversation-widget">
+      <Title  style={{ margin: "30px 0", height: "40px" }}level={3}>Save here what you want the system to remember for future conversations:</Title>
+      <Text> Anonymisation of the conversation will be done automatically.</Text>
+      <TextArea rows={10} />
+      <div className="save-conversation-widget__button">
+        <Button type="primary">Save</Button>
+      </div>
+    </div>
+  );
+};
+
 export default SaveConversationWidget;
--- frontend/src/components/ValidationWidget/index.jsx
+++ frontend/src/components/ValidationWidget/index.jsx
@@ -1,6 +1,6 @@
 import { Button, Flex, Progress } from "antd";
 import "./styles.css";
-import { Typography } from 'antd';
+import { Typography } from "antd";
 
 const { Title } = Typography;
 
@@ -8,7 +8,7 @@ const validation_titles = [
   "Quantitative Analysis",
   "Qualitative Analysis",
   "Similarity to previous conversations",
-]
+];
 
 const ValidationWidget = () => {
   // Fake validation scores
@@ -29,30 +29,27 @@ const ValidationWidget = () => {
 
   return (
     <div className="widget-container">
-      <Title level={4}>Validation Scores</Title>
+      <h2>Validation Scores</h2>
       <Flex justify="space-evenly">
         <div style={{ textAlign: "center" }}>
-          <Title level={5}>Accuracy</Title>
-          <Progress percent={accuracy * 100} type="circle" />
+          <Progress percent={accuracy * 100} type="circle" size={"small"}/>
+          <Title level={5}  style={{ margin: 0, height: "40px" }}>Accuracy</Title>
         </div>
         <div style={{ textAlign: "center" }}>
-          <Title level={5}>Confidence</Title>
-          <Progress percent={confidence * 100} type="circle" />
+          <Progress percent={confidence * 100} type="circle" size={"small"} />
+          <Title level={5}  style={{ margin: 0, height: "40px" }}>Confidence</Title>
         </div>
       </Flex>
       {scores.map((score, index) => (
         <div key={index} style={{ color: getColor(score) }}>
-          <Flex justify="space-between">
-            <Title level={5}>{validation_titles[index]}</Title>
+          <Flex justify="space-between" style={{ margin: 0, height: "40px" }}>
+            <h3>{validation_titles[index]}</h3>
             <span>{score}%</span>
           </Flex>
-          <Progress percent={score} showInfo={false} />
+          <Progress size={"small"} percent={score} showInfo={false} />
         </div>
       ))}
-      <button className="validate-button">
-        Evaluate Conversation
-      </button>
-     
+      <button className="validate-button">Evaluate Conversation</button>
     </div>
   );
 };
