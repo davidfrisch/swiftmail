70be32703ca0ff6951e5d88abb8915320d00b943
David Frischer <david.frischer@frischer.ch>
2024-11-18 12:51:05 +0100

    [dataset] Add urls and remove undergrad fake_emails

--- dataset/urls.py
+++ dataset/urls.py
@@ -2,11 +2,20 @@
 
 UCL_URLS = [
 "https://www.ucl.ac.uk/prospective-students/graduate/frequently-asked-questions",
-"https://www.ucl.ac.uk/prospective-students/undergraduate/frequently-asked-questions",
 "https://www.ucl.ac.uk/students/fees/pay-your-fees/how-to-pay",
 "https://www.ucl.ac.uk/students/fees-and-funding/pay-your-fees/fee-schedules/2024-2025/postgraduate-taught-fees-2024-2025",
 "https://www.ucl.ac.uk/students/fees-and-funding/pay-your-fees/fee-schedules/student-fee-status",
 "https://www.ucl.ac.uk/students/fees/pay-your-fees/when-to-pay",
 "https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/software-systems-engineering-msc",
 "https://www.ucl.ac.uk/prospective-students/graduate/why-choose-ucl/careers-and-employability",
+"https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/artificial-intelligence-and-medical-imaging-msc/2024",
+"https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/artificial-intelligence-biomedicine-and-healthcare-msc",
+"https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/artificial-intelligence-sustainable-development-msc/2024",
+"https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/computational-statistics-and-machine-learning-msc/2024",
+"https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/data-science-and-machine-learning-msc/2024",
+"https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/emerging-digital-technologies-msc/2024",
+"https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/geospatial-sciences-geographic-information-science-and-computing-msc/2024",
+"https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/integrated-machine-learning-systems-msc/2024",
+"https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/machine-learning-msc/2024",
+"https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees/spatio-temporal-analytics-and-big-data-mining-msc/2024",
 ]
